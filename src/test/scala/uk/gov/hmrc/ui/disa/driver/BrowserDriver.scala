ee745ac1b1f3f8fc38783765e42c65bedd48a103
Ibi Awesu <26194202+way2autotesting@users.noreply.github.com>
2025-09-18 20:25:04 +0100

    just for review

---
+++ src/test/scala/uk/gov/hmrc/ui/disa/driver/BrowserDriver.scala
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ */
+
+package uk.gov.hmrc.ui.disa.driver
+
+import com.typesafe.scalalogging.LazyLogging
+import org.openqa.selenium.WebDriver
+import uk.gov.hmrc.selenium.webdriver.Driver
+
+trait BrowserDriver extends LazyLogging {
+  logger.info(
+    s"Instantiating Browser: ${sys.props.getOrElse("browser", "'browser' System property not set. This is required")}"
+  )
+
+  implicit def driver: WebDriver = Driver.instance
+}
