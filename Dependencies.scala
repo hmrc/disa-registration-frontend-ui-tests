a14b4e05b3a6908ccfdb75bb94a483812545b73c
Ibi Awesu <26194202+way2autotesting@users.noreply.github.com>
2025-12-08 14:24:26 +0000

    fix jenkins issue v3

---
+++ Dependencies.scala
@@ -0,0 +1,12 @@
+import sbt.*
+
+object Dependencies {
+
+  val test: Seq[ModuleID] = Seq(
+    "ch.qos.logback"       % "logback-classic"     % "1.5.18" % Test,
+    "org.scalatest"       %% "scalatest"           % "3.2.19" % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner"      % "0.49.0" % Test,
+    "uk.gov.hmrc"         %% "domain-test-play-30" % "13.0.0" % Test,
+    "com.vladsch.flexmark" % "flexmark-all"        % "0.64.8" % Test
+  )
+}
