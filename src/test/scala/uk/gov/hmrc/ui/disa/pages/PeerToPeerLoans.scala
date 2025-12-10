1852dfa2655c5e9190a952aeaec234b123a88cea
lahiruwijesingheOC <lahiruwijesingheOC@users.noreply.github.com>
2025-12-10 17:40:02 +0000

    DFI-1459

---
+++ src/test/scala/uk/gov/hmrc/ui/disa/pages/PeerToPeerLoans.scala
@@ -0,0 +1,12 @@
+package uk.gov.hmrc.ui.disa.pages
+
+object PeerToPeerLoans extends BasePage {
+  val pageUrl: String   = s"$baseUrl/peer-to-peer-loans"
+  val pageTitle: String = "Which platform does your organisation use to offer peer-to-peer loans? - ISA products - disa-registration-frontend - GOV.UK"
+
+  def verifyPageUrl(): Boolean =
+    getCurrentUrl == pageUrl
+
+  def verifyPageTitle(title: String): Boolean =
+    getTitle == pageTitle
+}
