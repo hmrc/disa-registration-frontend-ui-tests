bb699f6217328e54e9c1dcfd010097d095bea048
lahiruwijesingheOC <lahiruwijesingheOC@users.noreply.github.com>
2025-12-10 17:43:27 +0000

    reformat the file

--- src/test/scala/uk/gov/hmrc/ui/disa/pages/PeerToPeerLoans.scala
+++ src/test/scala/uk/gov/hmrc/ui/disa/pages/PeerToPeerLoans.scala
@@ -2,7 +2,8 @@ package uk.gov.hmrc.ui.disa.pages
 
 object PeerToPeerLoans extends BasePage {
   val pageUrl: String   = s"$baseUrl/peer-to-peer-loans"
-  val pageTitle: String = "Which platform does your organisation use to offer peer-to-peer loans? - ISA products - disa-registration-frontend - GOV.UK"
+  val pageTitle: String =
+    "Which platform does your organisation use to offer peer-to-peer loans? - ISA products - disa-registration-frontend - GOV.UK"
 
   def verifyPageUrl(): Boolean =
     getCurrentUrl == pageUrl
