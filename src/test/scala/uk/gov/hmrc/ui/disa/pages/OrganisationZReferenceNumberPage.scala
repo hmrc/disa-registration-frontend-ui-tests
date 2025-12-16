9fb43148f33a519317a621c1eb4ec927d7f61802
Ibi Awesu <26194202+way2autotesting@users.noreply.github.com>
2025-12-16 10:32:49 +0000

    Add Organisation Z-Reference Number page object

---
+++ src/test/scala/uk/gov/hmrc/ui/disa/pages/OrganisationZReferenceNumberPage.scala
@@ -0,0 +1,12 @@
+package uk.gov.hmrc.ui.disa.pages
+
+object OrganisationZReferenceNumberPage extends BasePage {
+  val pageUrl: String   = s"$baseUrl/z-reference-number"
+  val pageTitle: String = " What is your organisation’s Z-reference number? - Organisation details - disa-registration-frontend - GOV.UK"
+
+  def verifyPageUrl(): Boolean =
+    getCurrentUrl == pageUrl
+
+  def verifyPageTitle(): Boolean =
+    getTitle == pageTitle
+}
