19683b02342d1e678d59ae29b0b03249bd1520b2
Ibi Awesu <26194202+way2autotesting@users.noreply.github.com>
2025-12-16 10:44:13 +0000

    format new files

--- src/test/scala/uk/gov/hmrc/ui/disa/pages/OrganisationZReferenceNumberPage.scala
+++ src/test/scala/uk/gov/hmrc/ui/disa/pages/OrganisationZReferenceNumberPage.scala
@@ -2,7 +2,8 @@ package uk.gov.hmrc.ui.disa.pages
 
 object OrganisationZReferenceNumberPage extends BasePage {
   val pageUrl: String   = s"$baseUrl/z-reference-number"
-  val pageTitle: String = " What is your organisation’s Z-reference number? - Organisation details - disa-registration-frontend - GOV.UK"
+  val pageTitle: String =
+    " What is your organisation’s Z-reference number? - Organisation details - disa-registration-frontend - GOV.UK"
 
   def verifyPageUrl(): Boolean =
     getCurrentUrl == pageUrl
