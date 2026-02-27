21c9a2aea2b6ed849f178f59d7c827512bba3c57
lahiruwijesingheOC <lahiruwijesingheOC@users.noreply.github.com>
2026-02-27 14:41:53 +0000

    DFI-1696

---
+++ src/test/scala/uk/gov/hmrc/ui/disa/pages/EligibilityToManageIsasPage.scala
@@ -0,0 +1,9 @@
+package uk.gov.hmrc.ui.disa.pages
+
+import uk.gov.hmrc.ui.disa.pages.DeclarationForIsaManagersPage.baseUrl
+
+object EligibilityToManageIsasPage extends BasePage {
+  val pageUrl: String   = s"$baseUrl/eligibility-to-manage-isas"
+  val pageTitle: String =
+    "Eligibility to manage ISAs - Certificates of authority - disa-registration-frontend - GOV.UK"
+}
