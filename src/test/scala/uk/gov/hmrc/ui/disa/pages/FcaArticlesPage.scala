6e8032a9a5d91ea62d596dc5a87e6122183d3907
lahiruwijesingheOC <lahiruwijesingheOC@users.noreply.github.com>
2026-03-02 17:46:44 +0000

    DFI-1698

---
+++ src/test/scala/uk/gov/hmrc/ui/disa/pages/FcaArticlesPage.scala
@@ -0,0 +1,9 @@
+package uk.gov.hmrc.ui.disa.pages
+
+import uk.gov.hmrc.ui.disa.pages.DeclarationForIsaManagersPage.baseUrl
+
+object FcaArticlesPage extends BasePage {
+  val pageUrl: String   = s"$baseUrl/fca-articles"
+  val pageTitle: String =
+    "FCA Articles that apply to your organisation - Certificates of authority - disa-registration-frontend - GOV.UK"
+}
