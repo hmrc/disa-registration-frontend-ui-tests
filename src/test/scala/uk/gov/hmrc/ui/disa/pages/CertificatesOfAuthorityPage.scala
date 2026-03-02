2f5771a8cefc5759308628ed6821fe60b2de9f60
lahiruwijesingheOC <lahiruwijesingheOC@users.noreply.github.com>
2026-03-02 17:07:56 +0000

    DFI-1697

---
+++ src/test/scala/uk/gov/hmrc/ui/disa/pages/CertificatesOfAuthorityPage.scala
@@ -0,0 +1,9 @@
+package uk.gov.hmrc.ui.disa.pages
+
+import uk.gov.hmrc.ui.disa.pages.DeclarationForIsaManagersPage.baseUrl
+
+object CertificatesOfAuthorityPage extends BasePage {
+  val pageUrl: String   = s"$baseUrl/certificates-of-authority"
+  val pageTitle: String =
+    "Certificates of authority which apply to your organisation - Certificates of authority - disa-registration-frontend - GOV.UK"
+}
