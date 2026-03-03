8b7d96f542fa16db71337fc0f5a0d5f09e7d8304
lahiruwijesingheOC <lahiruwijesingheOC@users.noreply.github.com>
2026-03-03 15:32:51 +0000

    DFI-1699

---
+++ src/test/scala/uk/gov/hmrc/ui/disa/pages/FinancialOrganisationPage.scala
@@ -0,0 +1,9 @@
+package uk.gov.hmrc.ui.disa.pages
+
+import uk.gov.hmrc.ui.disa.pages.DeclarationForIsaManagersPage.baseUrl
+
+object FinancialOrganisationPage extends BasePage {
+  val pageUrl: String   = s"$baseUrl/financial-organisation"
+  val pageTitle: String =
+    "Financial organisation - Certificates of authority - disa-registration-frontend - GOV.UK"
+}
