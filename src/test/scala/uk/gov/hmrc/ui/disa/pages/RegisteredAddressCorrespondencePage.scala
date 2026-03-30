7b2d92db3874cc8826e89bd333112016c3549353
lahiruwijesingheOC <lahiruwijesingheOC@users.noreply.github.com>
2026-03-30 12:35:31 +0100

    DFI-1141

---
+++ src/test/scala/uk/gov/hmrc/ui/disa/pages/RegisteredAddressCorrespondencePage.scala
@@ -0,0 +1,10 @@
+package uk.gov.hmrc.ui.disa.pages
+
+import uk.gov.hmrc.ui.disa.pages.SignOutPage.baseUrl
+
+object RegisteredAddressCorrespondencePage {
+  val pageUrl: String   = s"$baseUrl/registered-address-correspondence"
+  val pageTitle: String =
+    "Do you want us to use the registered address to send you letters? - Organisation details - Manage ISAs - GOV.UK"
+
+}
