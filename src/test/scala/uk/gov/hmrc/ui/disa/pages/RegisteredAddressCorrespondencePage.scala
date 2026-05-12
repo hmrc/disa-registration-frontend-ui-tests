2ddc810226a8306209583b5f0968a2e35574638f
aliharis99 <87179963+aliharis99@users.noreply.github.com>
2026-05-12 16:04:06 +0100

    RegisteredAddressCorrespondencePage now extends BasePage

--- src/test/scala/uk/gov/hmrc/ui/disa/pages/RegisteredAddressCorrespondencePage.scala
+++ src/test/scala/uk/gov/hmrc/ui/disa/pages/RegisteredAddressCorrespondencePage.scala
@@ -18,7 +18,7 @@ package uk.gov.hmrc.ui.disa.pages
 
 import uk.gov.hmrc.ui.disa.pages.SignOutPage.baseUrl
 
-object RegisteredAddressCorrespondencePage {
+object RegisteredAddressCorrespondencePage extends BasePage {
   val pageUrl: String   = s"$baseUrl/registered-address-correspondence"
   val pageTitle: String =
     "Do you want us to use the registered address to send you letters? - Organisation details - Manage ISAs - GOV.UK"
